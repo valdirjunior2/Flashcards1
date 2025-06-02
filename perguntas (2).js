dc738d7c799d31d21bf0c50be2c25fe3465a2bb5
valdir jr <valdir.feitosa.junior@escola.pr.gov.br>
2025-06-02 18:01:44 -0300

    Add files via upload

---
+++ perguntas (2).js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Histórias',
+    'O que foi o imperialismo?',
+    'O imperialismo foi a expansão das potências europeias, principalmente, em territórios da África, Àsia e Oceania, com o objetivo de dominar recursos naturais, mercados e estabelecer colonizações.'
+)
+
+criaCartao(
+    'Histórias',
+    'Quais as causas do imperialismo?',
+    'A busca por matérias-primas, novos mercados consumidores, prestígio nacional, avanço científico e tecnológico, e o desejo de difundir a cultura e religião europeia.'
+)
+
+criaCartao(
+    'Histórias',
+    'Quais países foram principais potências imperialistas no século x1x?',
+    'Reino Unido, França, Alemanha, Bélgica, Portugal e Itália foram as principais potências imperialistas, com destaque para o Reino Unico e a França.'
+)
+
+criaCartao(
+    'Histórias',
+    'O que foi a "Corrida pelo Império"?',
+    'Oi em ingles é HI (RAI)'
+)
