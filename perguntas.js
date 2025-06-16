d274b1f8d4d9869f6541ed1fb16c2eb522a11b21
valdir jr <valdir.feitosa.junior@escola.pr.gov.br>
2025-06-16 16:49:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'Histórias',
+    'O que foi o imperialismo?',
+    'O imperialismo foi a expansão das potências europeias, principalmente, em territórios da África, Àsia e Oceania, com o objetivo de dominar recursos naturais, mercados e estabelecer colonizações.'
+)
+
+criaCartao(
+    'Histórias',
+    'Quais as causas do imperialismo?',
+    'A busca por matérias-primas, novos mercados consumidores, prestígio nacional, avanço científico e tecnológico, e o desejo de difundir a cultura e religião europeia.'
+)
+
+criaCartao(
+    'Histórias',
+    'Quais países foram principais potências imperialistas no século x1x?',
+    'Reino Unido, França, Alemanha, Bélgica, Portugal e Itália foram as principais potências imperialistas, com destaque para o Reino Unico e a França.'
+)
+
+criaCartao(
+    'Histórias',
+    'O que foi a "Corrida pelo Império"?',
+    'A "Corrida pelo império"foi a rárida e intensiva divisão da Àfrica entre as potências europeias np final do século x1x, sem considerar as fronteiras ou culturas locais.'
+)
+criaCartao(
+     'Histórias',
+     'O imperialismo afetou as relações entre as potências?'
+     'Sim as potências competiam por territórios, o que gerou alianças e rivalidades, criando um clima de tensão internacional.'
+)
+
+criaCartao(
+    'Histórias',
+    'Qual foi o impacto do imperialismo nas populações locais?'
+    'As populações locais foram muitas vezes forçadas a abandonar sua terras, adotaram a cultura europeia, e sofreram com a violências e controle econômico das potências coloniais.'
+)
